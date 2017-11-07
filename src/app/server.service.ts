9975b6c46fb61ea0fa2d51f7103d174387a848d8
Pei <jzha471@aucklanduni.ac.nz>
2017-11-08 01:18:52 +1300

    236. Sending a PUT Request

--- src/app/server.service.ts
+++ src/app/server.service.ts
@@ -10,7 +10,10 @@ export class ServerService {
 
   storeServers(servers: any[]) {
     const headers = new Headers({'Content-Type': 'application/json'});
-    return this.http.post('https://udemy-ng-http-465f0.firebaseio.com/data.json',
+    // return this.http.post('https://udemy-ng-http-465f0.firebaseio.com/data.json',
+    //   servers,
+    //   {headers:headers});
+    return this.http.put('https://udemy-ng-http-465f0.firebaseio.com/data.json',
       servers,
       {headers:headers});
   }
